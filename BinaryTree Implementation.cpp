75ed94998257a98a8d10d6c2d6dcc85a8d9a29ea
PradnyaGokhale <158048489+PradnyaGokhale@users.noreply.github.com>
2024-06-20 19:49:18 +0530

    Update BinaryTree Implementation.cpp

--- BinaryTree Implementation.cpp
+++ BinaryTree Implementation.cpp
@@ -1,243 +1,243 @@
-//Simple Binary Tree Implementation
-#include <iostream>
+//Dictionary Implementation
+#include<iostream>
+#include<string.h>
 using namespace std;
-
-class node
+typedef struct node
 {
-public:
-    int data;
-    node *left, *right;
-    node()
-    {
-        data = 0;
-        left = NULL;
-        right = NULL;
-    }
+    char k[20];
+    char m[20];
+    class node*left;
+    class node*right;
+    
+}node;
+class Dict
+{
+    public:
+    node*root;
+    void create();
+    void disp(node*);
+    void insert(node*root,node*temp);
+    int search(node*,char[]);
+    int update(node*,char[]);
+    node*del(node*,char[]);
+    node*min(node*);
 };
 
-class btree
+void Dict::create()
 {
-public:
-    int flag=0;
-    node *root;
-    btree()
-    {
-        root = NULL;
-    }
-    void create()
-    {
-
-        int i, n, ch;
-
-        cout << "Enter total no. of nodes: ";
-        cin >> n;
-        for (i = 0; i < n; i++)
+    class node *temp;
+    int ch;
+    do{
+        temp=new node;
+        cout<<"\nEnter Keyword:";
+        cin>>temp->k;
+        cout<<"\nEnter Meaning:";
+        cin>>temp->m;
+        temp->left=NULL;
+        temp->right=NULL;
+        if(root==NULL)
         {
-            node *newnode = new node;
-            cout << "Enter the element: ";
-            cin >> ch;
-
-            newnode->data = ch;
-            if (root == NULL)
-            {
-                root = newnode;
-            }
-            else
-            {
-                insert(root, newnode);
-            }
-        }
-    }
-
-    void insert(node *temp, node *newnode)
-    {
-        if (temp->data < newnode->data)
-        {
-            if (temp->right == NULL)
-            {
-                temp->right = newnode;
-            }
-            else
-            {
-                insert(temp->right, newnode);
-            }
+            root=temp;
         }
         else
         {
-            if (temp->left == NULL)
-            {
-                temp->left = newnode;
-            }
-            else
-            {
-                insert(temp->left, newnode);
-            }
+            insert(root,temp);
         }
-    }
-    
-
-    
-    void min_value(node *temp)
-    {
-        if (temp != NULL)
-            while (temp->left != NULL)
-            {
-                temp = temp->left;
-            }
-        cout << temp->data;
-    }
-    
-
-    void find(node *root, int key)
-    {
-        if (root != NULL)
-        {
-            if (key == root->data)
-                cout << "Key found" << endl;
-
-            else if (key < root->data)
-                find(root->left, key);
-            else
-                find(root->right, key);
-        }
-        else{
-            cout<<"Key Not Found.";
-        }
-    }
-
-    int depth(node *root)
-    {
-        int hleft, hright;
-        if (root == NULL || ((root->left == NULL) && (root->right == NULL)))
-            return (0);
-        else
-        {
-            hleft = depth(root->left);
-            hright = depth(root->right);
-        }
-        if (hleft > hright)
-        {
-            return (hleft + 1);
-        }
-        else
-        {
-            return (hright + 1);
-        }
-    }
-
-    void inorder(node *temp)
-    {
-        if (temp != NULL)
-        {
-            inorder(temp->left);
-            cout << temp->data << "\t";
-            inorder(temp->right);
-        }
-    }
-    
-    void preorder(node *temp)
-    {
-        if (temp != NULL)
-        {
-            cout << temp->data << "\t";
-            preorder(temp->left);
-            preorder(temp->right);
-        }
-    }
-    
-    void postorder(node *temp)
-    {
-        if (temp != NULL)
-        {
-            postorder(temp->left);
-            postorder(temp->right);
-            cout << temp->data << "\t";
-        }
-    }
-
-    void disp_in()
-    {
-        int ch, choice;
-        cout << "\n\t1-Inorder\n\t2-Preorder\n\t3-Postorder\n";
-        cout << "\nEnter your choice: ";
-        cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            inorder(root);
-            break;
-
-        case 2:
-            preorder(root);
-            break;
-
-        case 3:
-            postorder(root);
-            break;
-
-        default:
-            cout << "\nInvalid choice!!!";
-            break;
-        }
+        cout<<"\nDo u want to Add more(y=1/y=0)";
+        cin>>ch;
+    }while(ch==1);
 }
-    
-    void swap(node* root)
+void Dict::insert(node*root,node*temp)
+{
+    if(strcmp(temp->k,root->k)<0)
     {
-        node* temp;
-        if(root!=NULL)
-        {
-            swap(root->left);
-            swap(root->right);
-            node*temp=root->left;
-            root->left=root->right;
-            root->right=temp;
-            }
+        if(root->left==NULL)
+            root->left=temp;
+        else
+        insert(root->left,temp);
     }
-};
-
+    else
+    {
+        if(root->right==NULL)
+        root->right=temp;
+        else
+        insert(root->right,temp);
+    }
+}
+void Dict::disp(node *root)
+{
+    if(root!=NULL)
+    {
+        disp(root->left);
+        cout<<"\n Key Word:"<<root->k;
+        cout<<"\tMeaning:"<<root->m;
+        disp(root->right);
+    }
+}
+int Dict::search(node*root,char k[20])
+{
+    int c=0;
+    while(root!=NULL)
+    {
+        c++;
+        if(strcmp(k,root->k)==0)
+        {
+            cout<<"\nNo of Comparisions"<<c;
+            return 1;
+        }
+        if(strcmp(k,root->k)<0)
+            root=root->left;
+        if(strcmp(k,root->k)>0)
+            root=root->right;
+        }
+        return -1;
+}
+int Dict::update(node *root,char k[20])
+{
+    while(root!=NULL)
+    {
+        if(strcmp(k,root->k)==0)
+        {
+            cout<<"\nEnter New Meaning of Key:"<<root->k;
+            cin>>root->m;
+            return 1;
+        }
+        if(strcmp(k,root->k)<0)
+            root=root->left;
+        if(strcmp(k,root->k)>0)
+            root=root->right;
+    }
+    return 1;
+}
+node *Dict::del(node *root,char k[20])
+{
+    node *temp;
+    if(root==NULL)
+    {
+        cout<<"\nElement Not Found";
+        return root;
+    }
+    if(strcmp(k,root->k)<0)
+    {
+        root->left=del(root->left,k);
+        return root;
+    }
+    if(strcmp(k,root->k)>0)
+    {
+        root->right=del(root->right,k);
+        return root;
+    }
+    if(root->right==NULL && root->left==NULL)
+    {
+        temp=root;
+        delete temp;
+        return NULL;
+    }
+    if(root->right==NULL)
+    {
+        temp=root;
+        root=root->left;
+        delete temp;
+        return root;
+    }
+    else if(root->left==NULL)
+    {
+        temp=root;
+        root=root->right;
+        delete temp;
+        return root;
+    }
+    temp=min(root->right);
+    strcpy(root->k,temp->k);
+    root->right=del(root->right,temp->k);
+    return root;
+}
+node *Dict::min(node *q)
+{
+    while(q->left!=NULL)
+    {
+        q=q->left;
+    }
+    return q;
+}
 int main()
 {
-    btree v;
-    int n;
-    char ch;
-    int value;
-
-    do
-    {
-        cout << "\n\n## MENU ##\n\n1)Insert";
-        cout << "\n2)Number of nodes in longest path from root\n3)Display\n4)Find minimum value\n5)Swap the elements\n6)Search the Elements\nEnter your choice:";
-        cin >> n;
-        switch (n)
+    int ch;
+    Dict d;
+    d.root=NULL;
+    do{
+        cout<<"\nEnter the option no. to perform:\n1.Create\n2.Display\n3.Search\n4.Update\n5.Delete\n--->";
+        cin>>ch;
+        switch(ch)
         {
-        case 1:
-            v.create();
-            cout << "Binary tree constructed successfully\n";
-            break;
-        case 2:
-            cout << "Number of nodes in longest path from root : " << (v.depth(v.root)+1)<< endl;
-            break;
-        case 3:
-            cout << "Inorder of tree" << endl;
-            v.disp_in();
-            break;
-        case 4:
-            cout << "Minimum value is:"<<endl;
-            v.min_value(v.root);
-            break;
-        case 5:
-            cout<<" Swap Elements:"<<endl;
-            v.swap(v.root);
-            v.inorder(v.root);
-            break;
-        case 6:
-            cout<<"Enter Elements to be searched:";
-            cin>>value;
-            v.find(v.root,value);
-            break;
+            case 1: d.create();
+                    break;
+            case 2:if(d.root==NULL)
+                    {
+                        cout<<"\nNo any Keyword!";
+                    }
+                    else
+                    {
+                        d.disp(d.root);
+                    }
+                    break;
+            case 3:if(d.root==NULL)
+                    {
+                        cout<<"\nDictionary is Empty.First add keywords then try again!";
+                    }
+                    else
+                    {
+                    cout<<"\nEnter Keyword which u want to search:";
+                    char k[20];
+                    cin>>k;
+                    
+                    if(d.search(d.root,k)==1)
+                        cout<<"\nKeyword Found!!";
+                    else
+                    cout<<"\nKeyword Not found!!";
+                    
+                    }
+                break;
+            case 4:if(d.root==NULL)
+                    {
+                        cout<<"\nDictionary is Empty.First add  keyworsd and then try again!";
+                    }
+                    else{
+                        cout<<"\nEnter the keyword Which meaning want you to update:";
+                        char k[20];
+                        cin>>k;
+                        if(d.update(d.root,k)==1)
+                        {
+                            cout<<"\nMeaning Updated";
+                            
+                        }
+                        else
+                        
+                        cout<<"\nMeaning Not Found";
+                    }
+                    break;
+            case 5:if(d.root==NULL)
+                    {
+                        cout<<"\nDictionary is empty.First add the keywords then try again";
+                    }
+                    else
+                    {
+                    cout<<"\nEnter the keyword which you want to delete";
+                        char k[20];
+                        cin>>k;
+                        if(d.root==NULL)
+                        {
+                            cout<<"\nNo any Keyword";
+                        }
+                        else
+                        {
+                            d.root=d.del(d.root,k);
+                            cout<<"Deleted Succesfully!!";
+                        }
+                    }
+                    break;
         }
-        cout << "\n\nContinue?(Y/N)" << endl;
-        cin >> ch;
-    } while (ch == 'Y' || ch == 'y');
-
+                    
+    }while(ch<=5);
     return 0;
 }
